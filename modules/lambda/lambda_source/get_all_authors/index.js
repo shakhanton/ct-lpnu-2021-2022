74331aaeb4f18db5b45f49073b1a3eefb45097b2
Anton Shakh <shakh@quintagroup.org>
2022-06-09 20:46:25 +0300

    update lambda get authors

--- modules/lambda/lambda_source/get_all_authors/index.js
+++ modules/lambda/lambda_source/get_all_authors/index.js
@@ -1,3 +1,23 @@
+// const AWS = require("aws-sdk");
+// const dynamodb = new AWS.DynamoDB({
+//   region: process.env.AWS_REGION,
+//   apiVersion: "2012-08-10"
+// });
+
+// exports.handler = (event, context, callback) => {
+//   const params = {
+//     TableName: process.env.TABLE_NAME
+//   };
+//   dynamodb.scan(params, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       callback(err);
+//     } else {
+//       callback(null, data);
+//     }
+//   });
+// };
+
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB({
   region: process.env.AWS_REGION,
@@ -8,13 +28,15 @@ exports.handler = (event, context, callback) => {
   const params = {
     TableName: process.env.TABLE_NAME
   };
-  dynamodb.scan(params, (err, data) => {
+  dynamodb.scan(params, (err, authors) => {
     if (err) {
       console.log(err);
       callback(err);
     } else {
-      callback(null, data);
+      callback(null, authors);
+      // callback(null, {
+        // body: JSON.stringify(authors)
+      // });
     }
   });
 };
-
